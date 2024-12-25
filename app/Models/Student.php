9ccab6fab67361c9738bf6408c8a3336344a26dd
Suweka <sansilunijas.22@uom.lk>
2024-12-25 14:00:15 +0530

    Define Relationships
    Overall Relationships
    Course and Enrollment (One-to-Many):
    
    One course can have many enrollments.
    This is defined in the Course model with hasMany(Enrollment::class).
    Enrollment to Student (Many-to-One):
    
    Each enrollment belongs to one student. (One Student can have many Enrolments)
    This is defined in the Enrollment model with belongsTo(Student::class).
    Enrollment to Course (Many-to-One):
    
    Each enrollment belongs to one course. (One course has many Enrolments)
    This is defined in the Enrollment model with belongsTo(Course::class).

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+class Student extends Model
 {
     use HasFactory;
 
-    public function enrollments()
-    {
+    public function enrollments(){
         return $this->hasMany(Enrollment::class);
     }
+
 
 }
