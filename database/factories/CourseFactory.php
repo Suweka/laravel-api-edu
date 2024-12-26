d30fdb088bd08ce95d2979d3a6384d7d3ba22a6a
Suweka <sansilunijas.22@uom.lk>
2024-12-26 11:39:58 +0530

    Add CourseFactory for generating dummy course data
    Implemented the `CourseFactory.php` to define logic for generating dummy data for the `courses` table. The factory includes the following fields:
    
    - `title`: Generates a random course title.
    - `description`: Generates a random course description.
    - `duration`: Generates a random integer (10-100) for course duration in hours.
    - `level`: Randomly assigns one of the course levels (Beginner, Intermediate, Advanced).

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -11,10 +11,14 @@ class CourseFactory extends Factory
      *
      * @return array
      */
+    
     public function definition()
-    {
-        return [
-            //
-        ];
-    }
+{
+    return [
+        'title' => $this->faker->sentence(3), // Generate a random course title
+        'description' => $this->faker->paragraph(), // Generate a random course description
+        'duration' => $this->faker->numberBetween(10, 100), // Generate a random duration (in hours)
+        'level' => $this->faker->randomElement(['Beginner', 'Intermediate', 'Advanced']), // Randomly assign a course level
+    ];
+}
 }
